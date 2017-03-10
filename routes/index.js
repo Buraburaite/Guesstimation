636953708cbd31b8512152a03088994a9241d935
Javier <jvrcueto@outlook.com>
2017-03-10 14:29:30 -0500

    settings-img works now for logout

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const indexRoutes = require('express').Router();
 const ensure = require('connect-ensure-login');
 
 indexRoutes.get('/', ensure.ensureLoggedIn(), (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/welcome');
+  }
+  console.log('Hello World!');
+
   res.render('index', {
     successMessage: req.flash('success'),
     title: 'Guesstimation',
