410a2531054c2b550a2f285aeec50e75960d1f85
Javier <jvrcueto@outlook.com>
2017-03-10 13:57:55 -0500

    lots of work on the index page and index.js, works lovely

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,27 @@
 $(document).ready((e) => {
-  $('.diff-row-cover').each( (index, el) => {
-    $(el).click((e) => {
-      $(el).hide();
+  //For each row of content
+  $('.diff-row').each( (index, thisRowEl) => {
+    //Save it's 'cover' in a variable
+    const cover = $(thisRowEl).next();
+
+    //When the cover is clicked
+    cover.click((e) => {
+
+      //Reveal all covers
+      $('.diff-row-cover').each( (index, coverEl) => {
+        $(coverEl).show();
+      });
+
+      //Hide all contents
+      $('.diff-row').each( (index, rowEl) => {
+        $(rowEl).hide();
+      });
+
+      //Reveal the current diff-row
+      $(thisRowEl).show();
+
+      //Then hide the current cover
+      cover.hide();
     });
   });
 });
