f252d1a3e994cefa997eb40f6b718db03f4743b9
Javier <jvrcueto@outlook.com>
2017-03-13 19:52:24 -0400

    almost ready to test

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -1,26 +1,20 @@
-// const Quiz = require('../../models/quiz-model.js');
-//
-const problems = null;
-
-// Quiz.find({}, (err, quizDocs) => {
-//
-//   // quizDocs.findEach
-// });
-
 $(document).ready((e) => {
 
+  //Save the answers in an object
   const answers = {};
-
   $('.problem-row').each( (index, el) => {
     answers['problem-' + index] = $(el).attr('answer');
   });
 
+  //Although a named color can be set,
+  //$(el).css will always return a rgb string
+  const white = 'rgb(255, 255, 255)';
   $('.choice-row').each( (index, el) => {
     const row = $(el);
 
     row.click((e) => {
 
-      if (row.css('color') === 'rgb(255, 255, 255)') {
+      if (row.css('color') === white) {
         row.css('color', 'gray');
       } else {
         row.css('color', 'white');
@@ -28,4 +22,35 @@ $(document).ready((e) => {
     });
   });
 
+  const responses = {};
+  $('.submit-btn').click( (e) => {
+    e.preventDefault();
+
+    const problems = $('.problem-row');
+    const totalProblems = problems.length;
+    let result = 0;
+
+    problems.each( (problemIndex, problemEl) => {
+      const problem = 'problem-' + problemIndex;
+      const possibles = [];
+
+      $('.choice-row').each( (rowIndex, choiceEl) => {
+        const choice = $(choiceEl);
+
+        if (choice.css('color') === white) {
+          possibles.push(choice.html());
+        }
+      });
+
+      const userAnswer = _.sample(possibles);
+      if (userAnswer === answers[problem]) {
+        result++;
+      }
+    });
+
+    console.log(result);
+  });
+
+
+  setTimeout(() => console.log(responses), 2000);
 });
